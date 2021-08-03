d25a477eb2612575e1c9ab6654d4b3559aab037e
DoaaDaban <doaadaban993@gmail.com>
2021-08-03 21:34:11 +0300

    sol1

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+class Weather extends React.Component{
+
+constructor(date, description ){
+super(props);
+
+date ='',
+description = ''
+
+}
+
+    render(){
+        return
+
+        <>
+        </>
+    }
+}
+
+export default Weather;
