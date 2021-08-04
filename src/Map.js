706facb196b5d7fa5491cdfe1b0ec11b7635fbac
DoaaDaban <doaadaban993@gmail.com>
2021-08-05 01:08:40 +0300

    solved

--- src/Map.js
+++ src/Map.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 class Map extends React.Component {
 
     render() {
 
         return (
-
             <>
                 {this.props.showCard &&
-                    <div class="card" style={{ width: '22rem', border: '1px solid black ', color: 'green' }}>
+                    <div class="card" style={{ width: '22rem', border: '1px solid black ', color: 'pink' }}>
                         <div class="card-header">
                             <h2> {this.props.displayName}</h2>
                         </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item" style={{ border: '1px solid black ', color: 'tomato' }}>
+                            <li class="list-group-item" style={{ border: '1px solid black ', color: 'green' }}>
                                 <h6>{this.props.lon}</h6></li>
-                            <li class="list-group-item" style={{ border: '1px solid black ', color: 'tomato' }}>
+                            <li class="list-group-item" style={{ border: '1px solid black ', color: 'green' }}>
                                 <h6>{this.props.lat}</h6></li>
                         </ul>
                     </div>
                 }
                 {this.props.showMap &&
                     <div class="card" style={{ width: "30rem" }}>
-                        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.props.lat},${this.props.lon}`} class="card-img-top" alt="map" />
+                        <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.e997da4c61621084f545d56f650156b1&center=${this.props.lat},${this.props.lon}`} class="card-img-top" alt="map" />
                         <div class="card-body">
-                            {/* <p class="card-text">
+                            <p class="card-text">
                                 {this.props.lon} , {this.props.lat}
-                            </p> */}
+                            </p>
                         </div>
                     </div>
                 }
---
+++ src/Movies.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+
+
+class Movies extends React.Component {
+
+    render() {
+        return (
+            <>
+                {
+                    this.props.showCard &&
+                    this.props.movies.map(element => {
+                        return (<Card style={{ border: 'groove', width: '860px' }} >
+                            <Card.Header as="h5">
+                                {element.title}
+                            </Card.Header>
+
+                            <Card.Img variant="top" src={element.poster_path} />
+
+                            {/* <img src={element.poster_path} alt=''/> */}
+
+                            {element.vote_average}
+                            {element.vote_count}
+                            {element.overview} 
+                            {element.popularity}
+                            {element.release_date}
+
+                
+                        </Card>
+                        )
+                    })
+                }
+
+
+            </>
+        )
+    }
+
+
+}
+
+export default Movies;
